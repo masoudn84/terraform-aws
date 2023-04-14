d1c254339199e1018b131379d87deefd2336318f
masoudn84 <46718248+masoudn84@users.noreply.github.com>
2023-04-14 12:05:01 +0330

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 variable "aws_access_key" {
-  default = "AKIA3DA5PWO3V4FQO5CE"
+  default = ""
 }
 variable "aws_secret_key" {
-  default = "n0iWzD2CwDRcJicz1TuxlWIQGL9hS58kb8wLoZO5"
+  default = ""
 }
 variable "aws_region" {
   default = "us-east-1"
